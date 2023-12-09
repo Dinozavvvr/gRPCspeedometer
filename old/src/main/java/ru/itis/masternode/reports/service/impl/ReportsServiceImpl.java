c3e3a2e2e944a0d764a9a532143ddacb2aa9ec0f
Dinar Shagaliev <dinar.shagaliev2002@gmail.com>
2023-12-09 19:09:48 +0300

    remove dir old

--- old/src/main/java/ru/itis/masternode/reports/service/impl/ReportsServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package ru.itis.masternode.reports.service.impl;
-
-import org.springframework.stereotype.Service;
-import ru.itis.masternode.reports.service.ReportsService;
-
-@Service
-public class ReportsServiceImpl implements ReportsService {
-}
