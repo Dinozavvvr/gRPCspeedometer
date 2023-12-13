0a3fd3dcd82e0e55073d4e9c018004b29944cc71
Антон Шеверда <sheverda@technokratos.com>
2023-12-13 18:28:48 +0300

    delete instancio

--- worker-node/src/main/java/ru/itis/workernode/service/impl/WorkerServiceImpl.java
+++ worker-node/src/main/java/ru/itis/workernode/service/impl/WorkerServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.itis.workernode.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.instancio.Instancio;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import ru.itis.workernode.client.WorkerClient;
@@ -12,12 +11,22 @@ import ru.itis.workernode.model.RequestConfig;
 import ru.itis.workernode.model.WorkerConfigInfo;
 import ru.itis.workernode.service.WorkerService;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class WorkerServiceImpl implements WorkerService {
+
+    private static final ExampleData exampleData = ExampleData.builder()
+            .id(1)
+            .name("name")
+            .lastName("lastName")
+            .age(22)
+            .date(LocalDate.now())
+            .height(5.5)
+            .build();
 
     private static final String MANY_DATA_OBJECTS = "Объектов должно быть не меньше, чем depthLevel * dataSize";
 
@@ -50,7 +59,7 @@ public class WorkerServiceImpl implements WorkerService {
                     .map(data -> {
                         if (FlowType.WATERFALL.equals(data.getConfig().getFlowType())) {
                             for (int i = 0; i < data.getConfig().getDepthLevel(); i++) {
-                                data.getData().add(Instancio.create(ExampleData.class));
+                                data.getData().add(exampleData);
                             }
                         }
                         return data;
@@ -58,7 +67,7 @@ public class WorkerServiceImpl implements WorkerService {
         }
         List<ExampleData> data = new ArrayList<>();
         for (int i = 0; i < requestConfig.getDataSize(); i++) {
-            data.add(Instancio.create(ExampleData.class));
+            data.add(exampleData);
         }
         return Mono.just(
                 WorkerConfigInfo.builder()
